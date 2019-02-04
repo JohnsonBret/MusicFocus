283e73beecc78d38d7e63563687d4dd6590d2a8c
Bret Main <bretaudio@gmail.com>
2019-02-04 10:23:29 -0800

    intial commit

---
+++ server.js
@@ -0,0 +1,88 @@
+//https://maialinonyc.com/
+//https://www.americarestaurant.ca/
+
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res)=>{
+    res.render('index.hbs', {
+        pageTitle: "Alpine Garden",
+        welcomeMessage: "Welcome to this homepage",
+        company: "Music Focus"
+    });
+});
+
+app.get('/history', (req, res)=>{
+    res.render('history.hbs', {
+        pageTitle: "History",
+        company: "Music Focus"
+    });
+});
+
+app.get('/owner', (req, res)=>{
+    res.render('owner.hbs', {
+        pageTitle: "Services",
+        company: "Music Focus"
+    });
+});
+
+app.get('/products', (req, res)=>{
+    res.render('products.hbs', {
+        pageTitle: "Products",
+        company: "Music Focus"
+    });
+});
+
+app.get('/contact', (req, res)=>{
+    res.render('contact.hbs', {
+        pageTitle: "Contact",
+        company: "Music Focus"
+    });
+});
+
+app.get('/bad', (req,res)=>{
+    res.send({
+        error : "Unable to handle Request"
+    });
+});
+
+app.post('/submit', (req, res)=>{
+    console.log(`Contact Name: ${req.body.contactName}
+                Contact Email: ${req.body.contactEmail}
+                Contact Number: ${req.body.contactNumber}
+                Contact Street ${req.body.contactStreet}
+                Contact City ${req.body.contactCity}
+                Contact Questions: ${req.body.contactQuestions}`);
+
+    res.render('contact.hbs', {
+        pageTitle: "Contact",
+        thanksMsg: "Thanks for contacting us!",
+        company: "Alpine Garden"
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`Server up on Port ${port}`);
+});
